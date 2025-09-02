d14225da1301a7f844d2b6edae8897f1ce5ab61a
Tこーた <@TEIKOTA.noreply.github.com>
2025-09-02 11:19:56 +0900

    Initial Commit

---
+++ YumemiPrefectureFortuneTests/YumemiPrefectureFortuneTests.swift
@@ -0,0 +1,17 @@
+//
+//  YumemiPrefectureFortuneTests.swift
+//  YumemiPrefectureFortuneTests
+//
+//  Created by 髙橋　広大 on 2025/09/02.
+//
+
+import Testing
+@testable import YumemiPrefectureFortune
+
+struct YumemiPrefectureFortuneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
