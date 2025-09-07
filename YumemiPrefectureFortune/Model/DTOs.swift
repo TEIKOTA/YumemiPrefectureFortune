d18e93173b3dd3aabf48ba0548c0f5ab02df169c
Tこーた <@TEIKOTA.noreply.github.com>
2025-09-07 21:36:28 +0900

    feat: YMD構造体を追加

--- YumemiPrefectureFortune/Model/DTOs.swift
+++ YumemiPrefectureFortune/Model/DTOs.swift
@@ -1,6 +1,12 @@
 import Foundation
 
 // MARK: - Request DTO
+/// year, month, day を持つJSONの日付表現のための構造体
+struct YMD: Codable, Equatable {
+    let year: Int
+    let month: Int
+    let day: Int
+}
 
 struct FortuneRequestDTO: Encodable {
     var name: String
