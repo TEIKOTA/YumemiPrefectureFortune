712e49e5d47d7d38aa27ce6d8fa0fd06e4e4df6c
Tこーた <@TEIKOTA.noreply.github.com>
2025-09-07 20:11:43 +0900

    feat: UserProfile構造体をSwiftDataに対応
    SwiftDataを使用してコアとなる`UserProfile`構造体を永続化
    
    ## 変更点
    
    - `struct`ではなく`@Model`マクロを持つ`final class`として`UserProfile`をリファクタリング
    - `@Attribute(.unique)`マクロでIDの一意性を保証
    - `private(set)`でプロパティをカプセル化し専用メソッド経由でのみ変更可能に
    - 安全な状態変更のため`update()`と`updateFortune()`メソッドを提供
    - 非永続的な`fortuneResult`プロパティに`@Transient`を使用

--- YumemiPrefectureFortune/Model/UserProfile.swift
+++ YumemiPrefectureFortune/Model/UserProfile.swift
@@ -1,12 +1,61 @@
 import Foundation
+import SwiftData
 
 
-struct UserProfile {
-    var id: UUID
-    var name: String
-    var birthday: Date
-    var bloodType: BloodType
-    var introduction: String?
-    var icon: Data?
-    var fortuneResult: FortuneResult
+@Model
+final class UserProfile {
+    
+    // MARK: - Properties
+    
+    /// 本来 id は let にすべきだが SwiftData (on Swift 6)の都合上varにしなければならない
+    /// そのため private(set) var 修飾子にしてファイル外からの変更を防ぐ
+    @Attribute(.unique) private(set) var id: UUID
+    
+    /// private(set) var 修飾子により専用メソッドを通じてのみ更新するようにする
+    private(set) var name: String
+    private(set) var birthday: Date
+    private(set) var bloodType: BloodType
+    private(set) var introduction: String?
+    private(set) var icon: Data?
+    
+    @Transient var fortuneResult: FortuneResult?
+    
+    // MARK: - Initializer
+
+    init(id: UUID,
+         name: String,
+         birthday: Date,
+         bloodType: BloodType,
+         introduction: String? = nil,
+         icon: Data? = nil,
+         fortuneResult: FortuneResult? = nil) {
+        
+        self.id = id
+        self.name = name
+        self.birthday = birthday
+        self.bloodType = bloodType
+        self.introduction = introduction
+        self.icon = icon
+        self.fortuneResult = fortuneResult
+    }
+    
+    // MARK: - Mutating Methods
+    
+    public func update(name: String,
+                       birthday: Date,
+                       bloodType: BloodType,
+                       introduction: String?,
+                       icon: Data?) {
+        
+        self.name = name
+        self.birthday = birthday
+        self.bloodType = bloodType
+        self.introduction = introduction
+        self.icon = icon
+    }
+    
+    public func updateFortune(with result: FortuneResult?) {
+        self.fortuneResult = result
+    }
+
 }
