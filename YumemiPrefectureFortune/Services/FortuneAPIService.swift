35e1bfc736597a7dbb92e02ab7069f7b0411b435
Tこーた <@TEIKOTA.noreply.github.com>
2025-09-08 14:54:26 +0900

    feat: FortuneAPIServiceの骨格を作成
    `FortuneAPIService`の基本的な構造を確立
    
    - サービスのインターフェースを定義する`FortuneAPIServiceProtocol`を導入
      - 依存性の注入（DI）とテストの容易化
    - このプロトコルに準拠した`FortuneAPIService`クラスと実装を待つ空の`fetchFortune`メソッドを追加

--- YumemiPrefectureFortune/Services/FortuneAPIService.swift
+++ YumemiPrefectureFortune/Services/FortuneAPIService.swift
@@ -8,3 +8,20 @@ enum APIError: Error {
     case badStatusCode(Int)
     case decodingError(Error)
 }
+
+// MARK: - Service Protocol
+
+/// 占いAPIサービスクラスが準拠するプロトコル
+/// これを定義することで、テスト時にモックを注入しやすくなる
+protocol FortuneAPIServiceProtocol {
+    func fetchFortune(from requestDTO: FortuneRequestDTO) async throws -> FortuneResult
+}
+
+ // MARK: - Service Implementation
+
+ /// 占いAPIとの通信を実際に担当するクラス
+final class FortuneAPIService: FortuneAPIServiceProtocol {
+    
+    func fetchFortune(from requestDTO: FortuneRequestDTO) async throws -> FortuneResult {
+    }
+}
