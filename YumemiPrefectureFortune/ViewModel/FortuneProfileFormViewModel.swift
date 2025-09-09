fec27b332526192cbe28328cb52b72aa3696b54a
Tこーた <@TEIKOTA.noreply.github.com>
2025-09-09 18:46:33 +0900

    feat: FortuneProfileFormViewModelにプロフィール保存とバリデーションロジックを実装
    FortuneProfileFormViewModelにプロフィールの保存機能と入力値のバリデーション機能を追加
    
    主な実装内容:
    - 必須項目（名前、誕生日、血液型）が入力されているかを確認するバリデーションを実装
    - 新規作成と既存プロフィールの更新を分岐し`UserProfile`オブジェクトを返す`save`メソッドを追加
    - バリデーションエラーを表現するための`ValidationError` enumを定義

--- YumemiPrefectureFortune/ViewModel/FortuneProfileFormViewModel.swift
+++ YumemiPrefectureFortune/ViewModel/FortuneProfileFormViewModel.swift
@@ -3,11 +3,15 @@ import SwiftData
 
 
 final class FortuneProfileFormViewModel: ObservableObject {
+    
     @Published var name: String?
     @Published var birthday: Date?
     @Published var bloodType: BloodType?
     @Published var introduction: String?
     @Published var icon: Data?
+    
+    // 存在するなら編集、nilなら新規
+    private var existingProfile: UserProfile?
     
     init(user: UserProfile?) {
         
@@ -18,5 +22,49 @@ final class FortuneProfileFormViewModel: ObservableObject {
         self.bloodType = user.bloodType
         self.introduction = user.introduction
         self.icon = user.icon
+        
+        self.existingProfile = user
     }
+    
+    func save() throws -> UserProfile {
+        
+        // 未入力の場合を弾く
+        guard let name = name, !name.isEmpty else {
+            throw ValidationError.missingField(field: "Name")
+        }
+        
+        guard let birthday = birthday else {
+            throw ValidationError.missingField(field: "birthday")
+        }
+        
+        guard let bloodType = bloodType else {
+            throw ValidationError.missingField(field: "bloodType")
+        }
+        
+        if let existingProfile {
+            
+            existingProfile.update(name: name,
+                                   birthday: birthday,
+                                   bloodType: bloodType,
+                                   introduction: introduction,
+                                   icon: icon)
+            return existingProfile
+            
+        }else {
+            
+            let newUserProfile = UserProfile(
+                name: name,
+                birthday: birthday,
+                bloodType: bloodType,
+                introduction: introduction,
+                icon: icon
+            )
+            return newUserProfile
+
+        }
+    }
+}
+
+enum ValidationError: Error {
+    case missingField(field: String)
 }
