2148b6c06b1f0ccae25645394c39dce53ba080d3
Tこーた <@TEIKOTA.noreply.github.com>
2025-09-14 12:15:05 +0900

    feat: FortuneDetailViewModelの初期プロパティを実装
    占い結果詳細画面の基本的な状態を管理するため、 `FortuneDetailViewModel` に `@Published` プロパティとイニシャライザを追加

--- YumemiPrefectureFortune/ViewModel/FortuneDetailViewModel.swift
+++ YumemiPrefectureFortune/ViewModel/FortuneDetailViewModel.swift
@@ -1,6 +1,17 @@
 import Foundation
+import UIKit
 
 
 final class FortuneDetailViewModel: ObservableObject {
+    
+    @Published var user: UserProfile
+    @Published var logoImage: UIImage?
+    @Published var headerImage: UIImage?
+    @Published var isLoading: Bool
+    
+    init(user: UserProfile) {
+        self.user = user
+        self.isLoading = false
+    }
     
 }
